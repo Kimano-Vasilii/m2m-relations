550f2cdc592565eebaee5599a1792d9bc96d39e0
Maksin Vasilii <Kimano.arb@mail.ru>
2024-06-16 09:30:11 +0500

    Initial

---
+++ articles/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Article, Scope
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
